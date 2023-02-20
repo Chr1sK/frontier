a7cdeab3917b2885ccb4293f4e2ffe60cbd37d11
gitea <gitea@abstruct.com.au>
2023-02-20 23:14:54 +1100

    Final Commit

--- robot.py
+++ robot.py
@@ -2,33 +2,75 @@ from inspect import isroutine
 import os
 import re
 
-placed = True
+#INITIALISATION
+placed = False
 x = 0
 y = 0
-heading = ""
+heading = ''
+directions = ['NORTH', 'EAST', 'SOUTH', 'WEST']
 
-
+#FUNCTIONS
 def move():
+    global x,y,heading
+    match(str(heading)):
+        case 'NORTH':
+            if x < 4:
+                x+=1
+            else: return 'X out of bounds'
+        case 'EAST':
+            if y < 4:
+                y+=1
+            else:
+                return 'Y out of bounds'
+        case 'SOUTH':
+            if x > 0:
+                x-=1
+            else:
+                return 'X out of bounds'
+        case 'WEST':
+            if y > 0:
+                y-=1
+            else:
+                return 'Y out of bounds'
     return "Success"
 
 def right():
+    global heading
+    heading = directions[directions.index(heading) + 1] if directions.index(heading) < 3 else directions[0]
     return "Success"
 
 def left():
+    global heading
+    heading = directions[directions.index(heading) - 1] if directions.index(heading) > 0 else directions[3]
     return "Success"
 
 def report():
+    global x, y, heading
     return '{}, {}, {}'.format(x,y,heading)
 
-def place(x, y, newHeading):
-    if x is None or y is None or newHeading is None:
-        return "Not valid place command"
+def place(newX, newY, newHeading):
+    try:
+        newX = int(newX)
+        newY = int(newY)
+        newHeading = str(newHeading[1:-1])
+    except:
+        return "Not valid place command arguments"
+    
+    if (newX < 0 or newX > 4) or (newY < 0 or newY > 4) or (newHeading not in directions):
+        return "Not valid place command arguments"
+    global x, y, heading, placed
+    x = newX
+    y = newY
+    heading = newHeading
+    placed = True
     return "Success"
+#END OF FUNCTIONS
 
+#MAIN FUNCTION
 def mainFunction():
+    os.system("clear")
     while True:
         command = input("Enter Command: ")
-        os.system("clear")
         if "place" not in command and placed is False:
             print ("Issue valid place command first")
         elif command == "move()":
@@ -40,24 +82,19 @@ def mainFunction():
         elif command == "report()":
             print(report())
         elif "place" in command:
-            pattern = re.search("^[place(]{1}[,]{2}[)]{1}$", command)
-            print(bool(pattern))
+            pattern = re.search("[^place(]{1}[)]{1}$", command)
             if pattern:
                 try:
                     argumentsString = command[command.index("(") + 1:-1].split(',')
+                    print(place(argumentsString[0], argumentsString[1], argumentsString[2]))
                 except:
-                    print("Invalid place() command")
-                print(argumentsString)
-                try: print(eval(command))
-                except Exception as e:
-                    print("error: " + e)
+                    print("Not valid place command")
             else:
                 print("Not valid place command")
-            
-            
         else:
             print("Not valid function, please try again")
         
+#CALL MAIN FUNCTION
 if __name__ == "__main__":
     mainFunction()
 
